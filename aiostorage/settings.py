56fbbca403736df4edda4b18e4a26e0a6391de9e
family-guy <guy@zorncapital.com>
2018-04-01 17:42:29 +0100

    Typo

--- aiostorage/settings.py
+++ aiostorage/settings.py
@@ -1,3 +1,3 @@
 BACKBLAZE_APP_KEY='<backblaze-app-key>'
 BACKBLAZE_ACCOUNT_ID='<backblaze-account-id>'
-BACKBLAZE_TEST_BUCKET_ID = '<backblaze-test-bucket-id'
+BACKBLAZE_TEST_BUCKET_ID = '<backblaze-test-bucket-id>'
