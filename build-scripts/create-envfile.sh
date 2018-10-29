4e6c824182ced3e86b17fcb354dd5b7aa6eb6d53
family-guy <guy@zorncapital.com>
2018-10-29 08:58:30 +0000

    Update gitignore; change var name cases

--- build-scripts/create-envfile.sh
+++ build-scripts/create-envfile.sh
@@ -5,16 +5,16 @@ python setup.py egg_info
 declare -a PIP_PACKAGES
 PIP_PACKAGES=($(< aiostorage.egg-info/requires.txt))
 
-OUTPUT=$'name: aiostorage\ndependencies:\n  - python=3.6.3\n  - pip:\n'
+output=$'name: aiostorage\ndependencies:\n  - python=3.6.3\n  - pip:\n'
 
-for PACKAGE in "${PIP_PACKAGES[@]}"
+for package in "${PIP_PACKAGES[@]}"
 do
-    OUTPUT=$OUTPUT'    - '$PACKAGE$'\n'
+    output=$output'    - '$package$'\n'
 done
 
 if [ ! -d dist ]; then
     mkdir -p dist
 fi
 cat <<EOF > dist/environment.yml
-$OUTPUT
+$output
 EOF
