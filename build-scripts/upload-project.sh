8357673b123bfa135f1d10df3835757d6c8b7e3b
family-guy <guy@zorncapital.com>
2018-10-30 20:26:15 +0000

    Correct ci workflow

--- build-scripts/upload-project.sh
+++ build-scripts/upload-project.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-PYPI="~/.pypirc"
-echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI
-echo "username=$PYPI_USERNAME" >> $PYPI
-echo "password=$PYPI_PASSWORD" >> $PYPI
+PYPI_CONFIG="~/.pypirc"
+echo $'[distutils]\nindex-servers = pypi\n[pypi]' > $PYPI_CONFIG
+echo "username=$PYPI_USERNAME" >> $PYPI_CONFIG
+echo "password=$PYPI_PASSWORD" >> $PYPI_CONFIG
 twine upload dist/*.tar.gz
