1103d84c6280e808df83c08d544eb56252840fc2
family-guy <guy@zorncapital.com>
2018-12-25 14:02:32 +0000

    Bump version

--- aiostorage/__init__.py
+++ aiostorage/__init__.py
@@ -24,7 +24,7 @@ from .blob_storage import BlobStorage
 from .exceptions import (BlobStorageMissingCredentialsError,
                          BlobStorageUnrecognizedProviderError,)
 
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 
 __all__ = ['BlobStorage', 'BlobStorageUnrecognizedProviderError',
            'BlobStorageMissingCredentialsError']
